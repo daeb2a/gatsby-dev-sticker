6cd9322ca34b28c52c5249f54157a1c11590f242
daeb2a <daeb2a@gmail.com>
2022-12-24 13:47:08 +0900

    10.10 Page Query

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,14 +1,28 @@
+import { graphql, PageProps } from "gatsby";
 import React from "react";
 import Layout from "../components/Layout";
 import Seo from "../components/Seo";
 
-function Blog() { 
+function Blog({ data }: PageProps<Queries.BlogTitlesQuery>) { 
    return (
     <Layout title="Blog">
       <p>The most recent news from my show.</p>
+      <ul>
+        {data.allFile.nodes.map((file, index) => <li key={index}>{file.name}</li>)}
+      </ul>
     </Layout>
    );
 }
+
+export const query = graphql`
+  query BlogTitles {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`;
 
 export const Head = () => <Seo title="Blog" />
 
